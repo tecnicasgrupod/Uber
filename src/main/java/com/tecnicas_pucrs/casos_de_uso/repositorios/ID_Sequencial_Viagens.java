06ca1ce9022820f0937e50b8fe9823d0f11923b1
Doyle Mello Barboza <doylemb@hotmail.com>
2019-12-03 00:53:24 -0300

    Id Viagens Comentarios Testes

---
+++ src/main/java/com/tecnicas_pucrs/casos_de_uso/repositorios/ID_Sequencial_Viagens.java
@@ -0,0 +1,18 @@
+package com.tecnicas_pucrs.casos_de_uso.repositorios;
+
+import com.tecnicas_pucrs.entidades.Viagem;
+
+public abstract class ID_Sequencial_Viagens {
+
+    private static int id_atual = -1;
+
+    public static int getNextId(){
+        if (id_atual == -1) {
+            for (Viagem v : RepoViagens.getInstance().getViagens()){
+                if (v.getId() >= id_atual) { id_atual = v.getId(); }
+            }
+        }
+        id_atual +=1;
+        return id_atual;
+    }
+}
